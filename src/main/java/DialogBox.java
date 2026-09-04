f6ac0c768ff66868c048ea808bdaaffb5e759202
LUZIYUE <luziyue451@gmail.com>
2026-09-04 12:49:06 +0800

    Complete JavaFX tutorial part 3

--- src/main/java/DialogBox.java
+++ src/main/java/DialogBox.java
@@ -3,11 +3,34 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.geometry.Pos;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Node;
 
 public class DialogBox extends HBox {
 
     private Label text;
     private ImageView displayPicture;
+
+    /**
+     * Flips the dialog box such that the ImageView is on the left and text on the right.
+     */
+    private void flip() {
+        this.setAlignment(Pos.TOP_LEFT);
+        ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
+        FXCollections.reverse(tmp);
+        this.getChildren().setAll(tmp);
+    }
+
+    public static DialogBox getUserDialog(String text, Image img) {
+        return new DialogBox(text, img);
+    }
+
+    public static DialogBox getDukeDialog(String text, Image img) {
+        var db = new DialogBox(text, img);
+        db.flip();
+        return db;
+    }
 
     public DialogBox(String s, Image i) {
         text = new Label(s);
@@ -20,4 +43,5 @@ public class DialogBox extends HBox {
 
         this.getChildren().addAll(text, displayPicture);
     }
+
 }
